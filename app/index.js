beb3efdc41162b5ebafaad0ae1d4b47256b7f9a3
Pankaj Jaiswal <pankajj112223333@gmail.com>
2024-02-19 22:45:55 +0530

    project setup

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View,Text } from "react-native";
+
+const Home = ()=>{
+    return(
+        <View>
+            <Text>Pankaj Jaiswal</Text>
+        </View>
+    )
+}
+
+export default Home;
