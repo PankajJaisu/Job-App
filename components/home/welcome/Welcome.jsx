02e16dd2b2c9b6d5e98100ae7a31094c324f7bc0
Pankaj Jaiswal <pankajj112223333@gmail.com>
2024-02-20 18:10:09 +0530

    welcom uidone

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Flatlist
+  } from 'react-native'
 import styles from './welcome.style'
-
+import {useRouter} from 'expo-router';
+import {icons,SIZES} from '../../../constants'
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+     <View style={styles.container}>
+      <Text style={styles.userName}>Hello Pankaj</Text>
+      <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+     </View>
+     <View style={styles.searchContainer}>
+      <View style={styles.searchWrapper}>
+        <TextInput
+        style={styles.searchInput}
+        value=""
+        onChange={()=>{}}
+        placeholder="What are you looking for"/>
+      
+      </View>
+      <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+        <Image
+         source={icons.search}
+         resizeMode='contain'
+         style={styles.searchBtnImage}
+        >
+
+        </Image>
+      </TouchableOpacity>
+     </View>
     </View>
   )
+  
 }
 
 export default Welcome
